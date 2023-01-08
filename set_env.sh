0a0fa61af6a79bf109685a91581199e877c3e959
YaTeewR <93889964+YaTeewR@users.noreply.github.com>
2023-01-08 14:58:18 +0100

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME postgres
+setx POSTGRES_PASSWORD admin1234
+setx POSTGRES_HOST database-instance.c7eduy8vdj0m.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB udagramdb
+setx AWS_BUCKET udagram-instaa
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET testing
+setx URL=http://localhost:8100  
